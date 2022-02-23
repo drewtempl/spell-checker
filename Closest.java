e8c1473a18a9e42360f7f9546832210ba6c21251
Drew Templeton <templetonag@appstate.edu>
2022-02-23 12:26:48 -0500

    Node

--- Closest.java
+++ Closest.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+import java.io.*;
 
 public class Closest {
 
-    private Node [][] grid;
+    public static void main(String [] args) {
 
+        Node [][] grid = new Node[100][100];
+
+        File f = new File("points.txt");
+
+        try {
+            Scanner scnr = new Scanner(f);
+            boolean status = true;
+
+            while (status) {
+                double x = scnr.nextDouble();
+                double y = scnr.nextDouble();
+
+                status = false;
+            }
+
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot open file " + f.getAbsolutePath());
+            System.out.println(e);
+        }
+    }
+
+    private static int hash(double k) {
+        return 1;
+    }
 }
