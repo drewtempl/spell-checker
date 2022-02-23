a976e99dbb62949515174f909d06e51cdc62d221
Drew Templeton <templetonag@appstate.edu>
2022-02-23 10:04:06 -0500

    Closest

---
+++ Closest.java
@@ -0,0 +1,6 @@
+
+public class Closest {
+
+    private Node [][] grid;
+
+}
---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    
+    private double data;
+    private Node next;
+
+    public Node (double data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
