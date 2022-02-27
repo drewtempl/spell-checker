99892754a85f9fa6d234624a948f6ab8b655603c
Drew Templeton <templetonag@appstate.edu>
2022-02-27 11:00:50 -0500

    optimized grid size

--- Closest.java
+++ Closest.java
@@ -3,13 +3,14 @@ import java.io.*;
 
 public class Closest {
 
-    static final int b = 100;
+    static final int b = 1000;
 
     public static void main(String[] args) {
 
         Node[][] grid = new Node[b][b];
 
         File f = new File("points.txt");
+        double min = 2;
 
         try {
             Scanner scnr = new Scanner(f);
@@ -18,56 +19,41 @@ public class Closest {
                 double x = scnr.nextDouble();
                 double y = scnr.nextDouble();
 
-                grid[hash(x)][hash(y)] = new Node(x, y, grid[hash(x)][hash(y)]);
+                int xCord = hash(x);
+                int yCord = hash(y);
 
-            }
-            double min = 3;
+                grid[xCord][yCord] = new Node(x, y, grid[xCord][yCord]);
 
-            for (int i = 0; i < b; i++) {
-                for (int j = 0; j < b; j++) {
-                    Node node = grid[i][j];
+                Node node = grid[xCord][yCord];
 
-                    while (node != null) {
-                        for (int row = i - 1; row <= i + 1 && row < b; row++) {
-                            if (row == -1)
-                                row++;
+                for (int i = xCord - 1; i <= xCord + 1 && i < b; i++) {
+                    if (i < 0)
+                        i = 0;
 
-                            else if (row == b)
-                                row--;
+                    for (int j = yCord - 1; j <= yCord + 1 && j < b; j++) {
+                        if (j < 0)
+                            j = 0;
 
-                            for (int col = j - 1; col <= j + 1 && col < b; col++) {
-                                if (col == -1)
-                                    col++;
-                                
-                                Node comp = grid[row][col];
+                        Node comp = grid[i][j];
 
-                                while (comp != null && comp != node) {
-                                    double dist = dist(comp, node);
+                        while (comp != null) {
+                            if (comp != node) {
+                                double dist = dist(comp, node);
 
-                                    if (dist < min) 
-                                        min = dist;
-
-                                    comp = comp.next;
-                                }
+                                if (dist < min)
+                                    min = dist;
                             }
+                            comp = comp.next;
+
                         }
 
-                        node = node.next;
                     }
-
                 }
+
             }
 
             System.out.println("The closest pair of points is: " + min);
-
-            // for (int i = 0; i < b; i++) {
-            // for (int j = 0; j < b; j++) {
-            // Node node = grid[i][j];
-
-            // if (node != null)
-            // System.out.println(node.xCord + " " + node.yCord);
-            // }
-            // }
+            scnr.close();
 
         } catch (FileNotFoundException e) {
             System.out.println("Cannot open file " + f.getAbsolutePath());
@@ -80,7 +66,7 @@ public class Closest {
     }
 
     private static double dist(Node a, Node b) {
-        return Math.sqrt(Math.pow((a.xCord - b.xCord), 2) + 
+        return Math.sqrt(Math.pow((a.xCord - b.xCord), 2) +
                 Math.pow((a.yCord - b.yCord), 2));
     }
 }
